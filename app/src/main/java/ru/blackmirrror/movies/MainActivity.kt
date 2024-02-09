8a8c321f3d22de7320a0c17053a720e38aebfec1
Ekaterina Petrova <ekaterinapetrova985@gmail.com>
2024-02-09 22:13:53 +0300

    Initial commit

---
+++ app/src/main/java/ru/blackmirrror/movies/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.blackmirrror.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
